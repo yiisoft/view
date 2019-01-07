5ac9b65d3bf6ec1199f31be427047898e040bd6b
Andrii Vasyliev <sol@hiqdev.com>
2019-01-07 13:32:50 +0200

    Added `view` DI config

--- config/common.php
+++ config/common.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'view' => [
+        '__class' => \yii\view\View::class,
+    ],
+
     \yii\view\Theme::class => \yii\di\Reference::to('theme'),
     'theme' => [
         '__class' => \yii\view\Theme::class,
