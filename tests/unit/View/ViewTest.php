81f2efae8eae0ec54ea3b55709152a07ef6171e5
Alexander Makarov <sam@rmcreative.ru>
2019-06-03 13:45:58 +0300

    Interface compatibility fixes, less app dependencies in tests 

--- tests/unit/View/ViewTest.php
+++ tests/unit/View/ViewTest.php
@@ -19,33 +19,29 @@ class ViewTest extends TestCase
 
     public function setUp()
     {
-        parent::setUp();
-
-        $this->mockApplication();
-        $this->testViewPath = $this->app->getAlias('@yii/tests/runtime').DIRECTORY_SEPARATOR.str_replace('\\', '_', get_class($this)).uniqid();
+        $this->testViewPath = sys_get_temp_dir() . '/'. str_replace('\\', '_', get_class($this)) . uniqid('', false);
         FileHelper::createDirectory($this->testViewPath);
     }
 
     public function tearDown()
     {
         FileHelper::removeDirectory($this->testViewPath);
-        parent::tearDown();
     }
 
     /**
      * @see https://github.com/yiisoft/yii2/issues/13058
      */
-    public function testExceptionOnRenderFile()
+    public function testExceptionOnRenderFile(): void
     {
         $view = $this->createView();
 
-        $exceptionViewFile = $this->testViewPath.DIRECTORY_SEPARATOR.'exception.php';
+        $exceptionViewFile = $this->testViewPath . DIRECTORY_SEPARATOR . 'exception.php';
         file_put_contents($exceptionViewFile, <<<'PHP'
 <h1>Exception</h1>
 <?php throw new Exception('Test Exception'); ?>
 PHP
         );
-        $normalViewFile = $this->testViewPath.DIRECTORY_SEPARATOR.'no-exception.php';
+        $normalViewFile = $this->testViewPath . DIRECTORY_SEPARATOR . 'no-exception.php';
         file_put_contents($normalViewFile, <<<'PHP'
 <h1>No Exception</h1>
 PHP
@@ -63,11 +59,10 @@ PHP
         $this->assertEquals($obInitialLevel, ob_get_level());
     }
 
-    public function testRelativePathInView()
+    public function testRelativePathInView(): void
     {
-        FileHelper::createDirectory($this->testViewPath.'/theme1');
-        $this->app->setAlias('@testviews', $this->testViewPath);
-        $this->app->setAlias('@theme', $this->testViewPath.'/theme1');
+        $themePath = $this->testViewPath . '/theme1';
+        FileHelper::createDirectory($themePath);
 
         $baseView = "{$this->testViewPath}/theme1/base.php";
         file_put_contents($baseView, <<<'PHP'
@@ -82,26 +77,26 @@ PHP
         file_put_contents($subView, $subViewContent);
 
         $view = $this->createView(new Theme([
-            '@testviews' => '@theme',
+            $this->testViewPath => $themePath,
         ]));
 
-        $this->assertSame($subViewContent, $view->render('@testviews/base'));
+        $this->assertSame($subViewContent, $view->render($this->testViewPath . '/base'));
     }
 
     /// FIXME
     /// copied from FileHelperTest without required fixes
-    public function testLocalizedDirectory()
+    public function testLocalizedDirectory(): void
     {
         $view = $this->createView();
         $this->createFileStructure([
             'views' => [
                 'faq.php' => 'English FAQ',
-                'de-DE'   => [
+                'de-DE' => [
                     'faq.php' => 'German FAQ',
                 ],
             ],
         ], $this->testViewPath);
-        $viewFile = $this->testViewPath.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'faq.php';
+        $viewFile = $this->testViewPath . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'faq.php';
         $sourceLanguage = 'en-US';
 
         // Source language and target language are same. The view path should be unchanged.
@@ -111,12 +106,12 @@ PHP
         // Source language and target language are different. The view path should be changed.
         $currentLanguage = 'de-DE';
         $this->assertSame(
-            $this->testViewPath.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$currentLanguage.DIRECTORY_SEPARATOR.'faq.php',
+            $this->testViewPath . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $currentLanguage . DIRECTORY_SEPARATOR . 'faq.php',
             $view->localize($viewFile, $currentLanguage, $sourceLanguage)
         );
     }
 
-    protected function createView(Theme $theme = null)
+    private function createView(Theme $theme = null): View
     {
         return new View($this->app, $theme ?: new Theme());
     }
