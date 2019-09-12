6b50511fd6eac86ef3238e66aacbe1ca2cd5ac13
Alexander Makarov <sam@rmcreative.ru>
2019-09-13 02:24:27 +0300

    Fix interface name

--- src/View/TemplateRendererInterface.php
+++ src/View/TemplateRendererInterface.php
@@ -2,7 +2,7 @@
 
 namespace Yiisoft\View;
 
-interface TemplateRenderer
+interface TemplateRendererInterface
 {
     /**
      * Renders a template file.
@@ -10,9 +10,9 @@ interface TemplateRenderer
      * This method is invoked by [[View]] whenever it tries to render a view.
      * Child classes must implement this method to render the given view file.
      *
-     * @param View   $view the view object used for rendering the file.
-     * @param string $template     the template file.
-     * @param array  $params   the parameters to be passed to the view file.
+     * @param View $view the view object used for rendering the file.
+     * @param string $template the template file.
+     * @param array $params the parameters to be passed to the view file.
      *
      * @return string the rendering result
      */
