76156aad42a14e5a4109a725f1d183a05ed5c3ae
terabytesoftw <terabytesoftw@gmail.com>
2019-10-24 12:24:09 -0300

    minor corrections.

--- src/Widget/Exception/InvalidConfigException.php
+++ src/Widget/Exception/InvalidConfigException.php
@@ -10,16 +10,4 @@ use Throwable;
  */
 class InvalidConfigException extends \Exception
 {
-    /**
-     * __construct.
-     *
-     * @param string $message
-     * @param integer $code
-     *
-     * @param Throwable $previous
-     */
-    public function __construct(string $message, int $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
