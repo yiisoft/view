dff122ca5d2f208770aa394cf44834de06110b18
Wilmer Arambula <terabytesoftw@gmail.com>
2020-10-28 13:30:21 -0300

    Fix minor corrections.

--- config/params.php
+++ config/params.php
@@ -2,10 +2,12 @@
 
 return [
     'yiisoft/view' => [
+        'basePath' => '@views',
+        'defaultParameters' => [],
         'theme' => [
             'pathMap' => [],
             'basePath' => '',
             'baseUrl' => '',
         ]
-    ]
+    ],
 ];
