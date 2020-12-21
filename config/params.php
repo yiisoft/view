f1400649dda6d46c939a2122d5c494e74947d763
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2020-12-21 15:14:25 +0300

    Fix config + Update dev dependencies (#115)

--- config/params.php
+++ config/params.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'yiisoft/view' => [
-        'basePath' => '@views',
+        'basePath' => '',
         'defaultParameters' => [],
         'theme' => [
             'pathMap' => [],
