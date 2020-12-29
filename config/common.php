eb88ee6bc20049355fbe316e36e5e076c51614da
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-29 19:34:28 -0300

    Fix config yiisoft/view.

--- config/common.php
+++ config/common.php
@@ -3,33 +3,15 @@
 declare(strict_types=1);
 
 use Yiisoft\Aliases\Aliases;
-use Yiisoft\View\Theme;
-use Yiisoft\View\WebView;
+use Yiisoft\View\View;
 
 /** @var array $params */
 
 return [
-    Theme::class => static function (Aliases $aliases) use ($params) {
-        $pathMap = [];
-
-        foreach ($params['yiisoft/view']['theme']['pathMap'] as $key => $value) {
-            $pathMap[$aliases->get($key)] = $aliases->get($value);
-        }
-
-        return new Theme(
-            $pathMap,
-            $params['yiisoft/view']['theme']['basePath'],
-            $params['yiisoft/view']['theme']['baseUrl']
-        );
-    },
-
-    WebView::class => [
-        '__class' => WebView::class,
+    View::class => [
+        '__class' => View::class,
         '__construct()' => [
-            'basePath' => static fn (Aliases $aliases) => $aliases->get($params['yiisoft/view']['basePath']),
-        ],
-        'setDefaultParameters()' => [
-            $params['yiisoft/view']['defaultParameters'],
+            'basePath' => fn (Aliases $aliases) => $aliases->get($params['yiisoft/view']['basePath']),
         ],
     ],
 ];
