2eb9f0e2d22f68189867aeabd4361be0a23e76cb
thenotsoft <thenotsoft@gmail.com>
2021-04-23 16:55:32 +0300

    cleanup

--- src/View.php
+++ src/View.php
@@ -299,7 +299,6 @@ class View implements DynamicContentAwareInterface
         if (strncmp($view, '//', 2) === 0) {
             // path relative to basePath e.g. "//layouts/main"
             $file = $this->basePath . '/' . ltrim($view, '/');
-
         }  elseif (($currentViewFile = $this->getRequestedViewFile()) !== false) {
             // path relative to currently rendered view
             $file = dirname($currentViewFile) . '/' . $view;
