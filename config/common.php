ca53272b4ff0d5ad2158b521aabadb3fc186eea4
Evgeniy Zyubin <mail@devanych.ru>
2021-05-27 13:52:28 +0300

    Add setCommonParameters() method to BaseView (#158)

--- config/common.php
+++ config/common.php
@@ -2,29 +2,20 @@
 
 declare(strict_types=1);
 
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\Log\LoggerInterface;
 use Yiisoft\Aliases\Aliases;
+use Yiisoft\Factory\Definition\DynamicReference;
 use Yiisoft\View\View;
 
 /** @var array $params */
 
 return [
-    View::class => static function (
-        EventDispatcherInterface $eventDispatcher,
-        LoggerInterface $logger,
-        Aliases $aliases
-    ) use ($params): View {
-        $view = new View(
-            $aliases->get($params['yiisoft/view']['basePath']),
-            $eventDispatcher,
-            $logger,
-        );
-
-        foreach ($params['yiisoft/view']['commonParameters'] as $id => $value) {
-            $view->setCommonParameter($id, $value);
-        }
-
-        return $view;
-    },
+    View::class => [
+        'class' => View::class,
+        '__construct()' => [
+            'basePath' => DynamicReference::to(static fn (Aliases $aliases) => $aliases->get($params['yiisoft/view']['basePath'])),
+        ],
+        'setCommonParameters()' => [
+            $params['yiisoft/view']['commonParameters'],
+        ],
+    ],
 ];
