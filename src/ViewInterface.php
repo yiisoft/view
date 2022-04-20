dbf82fa1790b0325638551d53519000b4ff51d4c
thenotsoft <thenotsoft@gmail.com>
2022-04-20 16:09:00 +0300

    add withBasePath to ViewInterface

--- src/ViewInterface.php
+++ src/ViewInterface.php
@@ -96,6 +96,15 @@ interface ViewInterface
     public function withLanguage(string $language): self;
 
     /**
+     * Returns a new instance with specified base path to the view directory.
+     *
+     * @param string $basePath The base path to the view directory.
+     *
+     * @return static
+     */
+    public function withBasePath(string $basePath): self;
+
+    /**
      * Set the specified language code.
      *
      * @param string $language The language code.
